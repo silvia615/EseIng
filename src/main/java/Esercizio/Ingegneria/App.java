3c4975fba0a237e584c1c9fa1bad1bf8be3f7257
Silvia Mollica <silvia.mollica18@gmail.com>
2020-10-28 17:33:41 +0100

    Primo Commit

---
+++ src/main/java/Esercizio/Ingegneria/App.java
@@ -0,0 +1,13 @@
+package Esercizio.Ingegneria;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public static void main(String[] args) {
+		MyMath math = new MyMath();
+		System.out.println(math.primeNumbersGeneretor(20));
+	}
+}
---
+++ src/main/java/Esercizio/Ingegneria/MyMath.java
@@ -0,0 +1,31 @@
+package Esercizio.Ingegneria;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class MyMath {
+	public List<Integer> primeNumbersGeneretor(int n) {
+		List<Integer> primeNumbers = new LinkedList<>();
+		if(n>=2) {
+			primeNumbers.add(2);
+		}
+		 for (int i = 3; i<= n; i+=2) {
+			 if(isPrime(i)) {
+				 primeNumbers.add(i);
+			 }
+		 }
+		 return  primeNumbers;
+	}
+	
+	public boolean isPrime(int number) {
+		if(number <2 ) {
+			return false;
+		}
+		 for (int i = 2; i*i < number; i++) {
+			 if(number % i == 0) {
+				 return false;
+			 }
+		 }
+		 return true;
+	}
+}
